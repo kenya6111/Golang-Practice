3b3d87f56f9438dd0db3f3001481aabd9273fe58
kenya6111 <kenyanke6111@gmail.com>
2025-04-19 01:06:19 +0900

    section11

---
+++ section11/63/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type User struct {
+	Name string
+	Age  int
+}
+
+// 構造体のメソッドは、任意の型に特化した関数を定義するための仕組み
+func (u User) sayName() { // メソッド名の前にレシーバを書く
+	fmt.Println(u.Name)
+
+}
+
+func (u User) SetName(name string) {
+	u.Name = name
+}
+func (u *User) SetName2(name string) { // レシーバをポインタ型にしているので参照私できるようになった
+	u.Name = name
+}
+func main() {
+	user1 := User{Name: "user1"}
+	user1.sayName()
+
+	user1.SetName("A")
+	user1.sayName()
+
+	user1.SetName2("A")
+	user1.sayName()
+}
