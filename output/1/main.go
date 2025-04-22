8a3b64c25b9b365d39b0fc2f08868ad6a0da14b0
kenya6111 <kenyanke6111@gmail.com>
2025-04-22 21:22:57 +0900

    アウトプット課題1

--- output/1/main.go
+++ output/1/main.go
@@ -3,18 +3,24 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
 func main() {
 	// sl := []interface{}{1, "2", 10, "11"}// interface型は、「interface{}」と書くので、先はNG。
 	sl := []interface{}{1, "2", 10, "11"}
 	for _, v := range sl {
+
 		// fmt.Printf("%T\n", v)
-		fmt.Println(reflect.TypeOf(v))
+		rv := reflect.ValueOf(v)
+		if rv.Kind() == reflect.String {
+			// fmt.Printf("%02d\n", v)
 
-		fmt.Printf("%T\n", reflect.TypeOf(v))
-		// fmt.Println(v)
-
-		// fmt.Println(fmt.Sprintf("%d", v))
+			i, _ := strconv.Atoi(v.(string))
+			fmt.Printf("%02d\n", i)
+		}
+		if rv.Kind() == reflect.Int {
+			fmt.Println(v)
+		}
 	}
 }
