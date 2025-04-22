d7997a2307a88be1e0181b96622890078e1b38d1
kenya6111 <kenyanke6111@gmail.com>
2025-04-22 21:23:45 +0900

    section16

---
+++ section16/77/main.go
@@ -0,0 +1,50 @@
+package main
+
+func main() {
+	// os.Exit(1)
+	// fmt.Println("start")
+
+	// _, err := os.Open("aaa.txt")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	// fmt.Println(os.Args[0])
+	// fmt.Println(os.Args[1])
+	// fmt.Println(os.Args[2])
+	// fmt.Println(os.Args[3])
+
+	// fmt.Printf("length=%d\n", len(os.Args))
+
+	// for i, v := range os.Args {
+	// 	fmt.Println(i, v)
+	// }
+
+	// f, _ := os.Create("test.txt")
+	// f.Write([]byte("hello\n"))
+	// f.Write([]byte("aaasasa\n"))
+	// f.WriteAt([]byte("Golang"), 1)
+	// f.Seek(0, os.SEEK_END)
+	// f.WriteString("Stringggggg")
+
+	// f, err := os.Open("foo.txt")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	// defer f.Close()
+
+	// bs := make([]byte, 128)
+
+	// n, err := f.Read(bs)
+	// fmt.Println(n)
+	// fmt.Println(string(n))
+
+	// bs2 := make([]byte, 128)
+
+	// nn, err := f.ReadAt(bs2, 10)
+
+	// fmt.Println(nn)
+	// fmt.Println(string(bs2))
+
+}
