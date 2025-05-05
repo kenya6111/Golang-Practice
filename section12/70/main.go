1f93a6127ee3c6a2be65a77f24284adea72c005c
kenya6111 <kenyanke6111@gmail.com>
2025-05-05 22:21:44 +0900

    section12

--- section12/70/main.go
+++ section12/70/main.go
@@ -2,21 +2,25 @@ package main
 
 import "fmt"
 
-type Person struct {
+type Person struct { // OK
 	Name string
 	Age  int
 }
 
-type Car struct {
+type Car struct { // OK
 	Number string
 	Model  string
 }
 
-func (p *Person) ToString() string {
+// ：なぜ「レシーバ（receiver）」と呼ばれるのか？
+// それは、その関数を「受け取る（= receive）」型を指定するから
+
+// *Person 型のポインタレシーバを持つ ToString メソッド と呼べる
+func (p *Person) ToString() string { // OK
 	return fmt.Sprintf("Name=%v,name=%v", p.Name, p.Age)
 }
 
-func (c *Car) ToString() string {
+func (c *Car) ToString() string { // OK
 	return fmt.Sprintf("Number=%v,Model=%v", c.Number, c.Model)
 }
 
@@ -24,6 +28,11 @@ type Stringfy interface {
 	ToString() string
 }
 
+// interface とは？
+// → 「こういうメソッドを持ってる型だけ通してOK！」という「型のルール」
+
+// type MyInt int      // int をベースにした新しい型 MyInt を作る
+// type User struct{}  // struct の定義に User という名前をつける
 func main() {
 
 	vs := []Stringfy{
