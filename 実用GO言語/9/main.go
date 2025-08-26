ac5d2698279b4ca5963e5af8500f93cd19481bf1
kenya6111 <kenyanke6111@gmail.com>
2025-08-26 20:39:43 +0900

    実用9

--- 実用GO言語/9/main.go
+++ 実用GO言語/9/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	kyu_5 "jituyou/9-5"
+
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 func main() {
 	// kyu_1.Test_9_1()
+	// kyu_2.Test_9_2()
+	// kyu_2_1.Test_9_2_1()
+	kyu_5.Test_9_5()
 
 }
